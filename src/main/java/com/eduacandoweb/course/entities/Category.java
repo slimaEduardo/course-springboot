bfbebafeb9ff772f3796a693c58118256a0af451
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-09-04 17:04:04 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/eduacandoweb/course/entities/Category.java
+++ src/main/java/com/eduacandoweb/course/entities/Category.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -22,7 +23,7 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	@Transient
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 
 	public Category() {
