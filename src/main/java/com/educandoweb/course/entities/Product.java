11f166a567f46b9f110777a2ce8350629e8bd0ed
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-09-05 16:00:24 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -12,10 +12,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 
@@ -37,6 +40,9 @@ public class Product implements Serializable {
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
+	@OneToMany(mappedBy = "id.product")
+	@Fetch(FetchMode.JOIN)
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 	}
@@ -94,7 +100,16 @@ public class Product implements Serializable {
 		return categories;
 	}
 
-		
+	
+	@JsonIgnore	
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+ 		return set;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
